3264130f59390d2a3a983897cf8e5d6d95da922f
DewashishDubey <dewasishdubey@gmail.com>
2023-11-07 21:16:02 +0530

    Initial Commit

---
+++ src/components/questions_data/data.js
@@ -0,0 +1,73 @@
+export default [
+    {
+        question: 'A finite automaton accepts which type of language?',
+        answers: [
+            'Type 0',
+            'Type 1',
+            'Type 2',
+            'Type 3'
+        ],
+        correct: 4
+    },
+
+    {
+        question: 'Which among the following are incorrect regular identities?',
+        answers: [
+            'εR=R',
+            'ε*=ε',
+            'Ф*=ε',
+            'RФ=R'
+        ],
+        correct: 4
+    },    
+    {
+        question: 'Simplify the following regular expression: ε+1*(011) *(1*(011) *) *',
+        answers: [
+            '(1+011)*',
+            '(1*(011) *)', 
+            '(1+(011) *) *', 
+            '(1011) *'
+        ],    
+        correct: 1
+    },    
+    {
+        question: 'If the number of steps required to solve a problem is O(nk), then the problem is said to be solved in:',
+        answers: [
+            'non-polynomial time',
+            'polynomial time', 
+            'infinite time', 
+            'none of the mentioned'
+        ],    
+        correct: 2
+    },
+    {
+        question: '_____ can be done while more than one element needs to be returned from a component?',
+        answers: [
+            'Abstraction',
+            'Packing', 
+            'Insulation', 
+            'Wrapping'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'A turing machine is a',
+        answers: [
+            'real machine',
+            'abstract machine', 
+            'more than one option is correct', 
+            'hypothetical machine'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'A turing machine operates over:',
+        answers: [
+            'finite memory tape',
+            'infinite memory tape', 
+            'depends on the algorithm', 
+            'none of the mentioned'
+        ],    
+        correct: 2
+    }
+]
